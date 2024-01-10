3c4b9c67d0bccdcd35497611bbcf07eefc0e1407
Integralist <mark.mcdx@gmail.com>
2024-01-10 12:00:58 +0000

    feat: stderr

--- main.go
+++ main.go
@@ -4,26 +4,64 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
 
 	"github.com/fatih/color"
 )
 
+var (
+	alt bool
+	mu  sync.Mutex
+)
+
 func main() {
+	// Read from standard input
+	go readInput()
+
+	// Read from standard error
+	go readError()
+
+	// Wait for both goroutines to finish
+	select {}
+}
+
+func readInput() {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var alt bool
 	for scanner.Scan() {
-		if alt {
-			color.Red(scanner.Text())
-			alt = false
-			continue
-		}
-		color.Green(scanner.Text())
-		alt = true
+		mu.Lock()
+		alt = alternate(alt, scanner.Text())
+		mu.Unlock()
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error reading standard input:", err)
 		os.Exit(1)
 	}
+}
+
+func readError() {
+	scanner := bufio.NewScanner(os.Stderr)
+
+	for scanner.Scan() {
+		mu.Lock()
+		alt = alternate(alt, scanner.Text())
+		mu.Unlock()
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading standard error:", err)
+		os.Exit(1)
+	}
+}
+
+func alternate(alt bool, txt string) bool {
+	if alt {
+		color.Red(txt)
+		alt = false
+		return alt
+	}
+	color.Green(txt)
+	alt = true
+	return alt
 }
