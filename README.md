fed475d38c3f7a805f081acb6ab82d5b2cedecbb
Integralist <mark.mcdx@gmail.com>
2024-01-11 10:49:44 +0000

    feat: -pattern flag

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 Simple program that streams data piped into stdin and colors each line with alternative colors.
 
-## Example
+## Examples
 
 ```
 seq 1 5 | go run main.go
 ```
 
 ![](./alt-rows.png)
+
+```
+seq 1 5 | go run main.go -pattern '(?:[5-9]|10)'
+```
+
+![](./alt-rows-pattern.png)
+
+## Install
+
+```
+go build
+sudo cp ./alt-rows /usr/local/bin/
+```
--- main.go
+++ main.go
@@ -2,33 +2,58 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/mgutz/ansi"
 )
 
 var (
-	g = ansi.ColorFunc("green+bh")
-	r = ansi.ColorFunc("red+bh")
+	cg = ansi.ColorFunc("green+bh")
+	cr = ansi.ColorFunc("red+bh")
 )
 
 func main() {
+	var r *regexp.Regexp
+	pattern := patternFlag()
+	if pattern != "" {
+		r = regexp.MustCompile(pattern)
+	}
+
+	if err := scan(r); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading standard input:", err)
+		os.Exit(1)
+	}
+}
+
+func patternFlag() string {
+	var pattern string
+	flag.StringVar(&pattern, "pattern", "", "specify regex pattern for lines to apply alternating rows to (defaults to all input)")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: alt-rows [-pattern=<regex>]")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	return pattern
+}
+
+func scan(r *regexp.Regexp) error {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	var alt bool
 	for scanner.Scan() {
-		if alt {
-			fmt.Println(r(scanner.Text()))
-			alt = false
-			continue
+		if r == nil || r.Match(scanner.Bytes()) {
+			if alt {
+				fmt.Println(cr(scanner.Text()))
+				alt = false
+				continue
+			}
+			fmt.Println(cg(scanner.Text()))
+			alt = true
 		}
-		fmt.Println(g(scanner.Text()))
-		alt = true
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error reading standard input:", err)
-		os.Exit(1)
-	}
+	return scanner.Err()
 }
